5c60c7743c24e9e668a44f83ecce2a24045adb45
amrutha-2002 <74000712+amrutha-2002@users.noreply.github.com>
2021-05-30 19:58:29 +0530

    Add files via upload

---
+++ part1/e-1.7/App.js
@@ -0,0 +1,60 @@
+import React, { useState } from 'react'
+const Button = ({text, handleClick}) => <button onClick={handleClick}>{text}</button>;
+const Stati = ({notes, num}) => {
+    return (
+        <tr>
+            <td>{notes}</td>
+            <td>{num}</td>
+        </tr>
+    );
+};
+const Statistic = ({good, neutral, bad}) => {
+    
+    return (
+        <div>
+            
+                <table>
+                    <tbody>
+                        <Stati notes='Good' num={good} />
+                        <Stati notes='Neutral' num={neutral} />
+                        <Stati notes='Bad' num={bad} />
+                        <Stati notes='All' num={good+neutral+bad} />
+                        <Stati notes='Average' num={(good - bad) /good+neutral+bad } />
+                        <Stati notes='Positive' num={(good / (good+neutral+bad)) * 100} />
+                    </tbody>
+                </table>
+                 :
+                  <p>No feedback given.</p>              
+        
+        </div>
+    ); 
+    
+};
+const App = () => {
+    const heading = 'give feedback'
+    const head = 'statistics'
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+  
+  return (
+        <div>
+       <h1>{heading}</h1>
+       
+        <button onClick={() => setGood(good + 1)}>
+          good
+        </button>
+        <button onClick={() => setNeutral(neutral + 1)}>
+        neutral
+        </button>
+        <button onClick={() => setBad(bad + 1)}>
+        bad  
+         </button>
+        <h1>{head}</h1>
+        <Statistic good={good} neutral={neutral} bad={bad} />
+        
+</div>
+    )
+};
+
+export default App
